d785bc9715e0c407959513a6a9132bd95796c2dd
Walter van Niftrik <walter@vanniftrik-it.nl>
2015-03-01 12:23:04 +0100

    Switch build system to CMake

--- src/bin/toluabind.h
+++
@@ -1,8 +0,0 @@
-/*
-** Lua binding: tolua
-** Generated automatically by tolua++-1.0.93-lua52 on Sat Feb 28 23:09:35 2015.
-*/
-
-/* Exported function */
-TOLUA_API int  tolua_tolua_open (lua_State* tolua_S);
-
