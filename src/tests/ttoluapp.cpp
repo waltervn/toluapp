540d813a9c887f7c4b8b31830f2150423e1431d5
Walter van Niftrik <walter@vanniftrik-it.nl>
2015-03-03 09:39:24 +0100

    Hook up toluapp test and fix test building on Linux

--- src/tests/ttoluapp.cpp
+++ src/tests/ttoluapp.cpp
@@ -4,29 +4,33 @@ extern "C" {
 #include "lauxlib.h"
 }
 
-#include "tclass.h"
+#include "ttoluapp.h"
 
 //Test::Tst_A* Test::Tst_A::last;
 //Test::Tst_B* Test::Tst_B::last;
 //Test::Tst_C* Test::Tst_C::last;
 
-extern "C" {
-	int  tolua_tclass_open (lua_State*);
-}
+//extern "C" {
+	int  tolua_ttoluapp_open (lua_State*);
+//}
 
 int main ()
 {
+	int errcode = 0;
 	Test::Tst_B* b = new Test::Tst_B;         // instance used in Lua code
 
-	lua_State* L = lua_open();
+	lua_State* L = luaL_newstate();
 	luaL_openlibs(L);
-	tolua_tclass_open(L);
+	tolua_ttoluapp_open(L);
 
-	luaL_dofile(L,"tclass.lua");
+	if (luaL_dofile(L,"ttoluapp.lua") != 0) {
+		fprintf(stderr, "%s", lua_tostring(L,-1));
+		errcode = 1;
+	}
 
 	lua_close(L);
 
 	delete b;
-	return 0;
+	return errcode;
 }
 
